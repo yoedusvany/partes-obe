4f182a4cc3d9c12adcc83f6a5bbdaad917301dd1
Yoedusvany <yoedusvany@gmail.com>
2019-11-01 16:55:36 -0400

    con autenticacion

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,14 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
-Route::resource('services', 'ServicesController');
-Route::resource('sucursales', 'SucursalController');
-Route::resource('ordenes', 'OrdenController');
+Route::apiResource('services', 'ServicesController');
+Route::apiResource('sucursales', 'SucursalController');
+Route::apiResource('ordenes', 'OrdenController');
 
 Route::get('getAllRangos', 'OrdenController@getAllRangos');
 Route::get('getAllRangosEjecutados', 'OrdenController@getAllRangosEjecutados');
